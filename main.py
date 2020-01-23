22487b2f657105365706e37841adead457b6d37e
joro2404 <joro2404@abv.bg>
2020-01-23 21:30:53 +0200

    creating login/register form

---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from flask import render_template, request, redirect, url_for, jsonify
+import json
+
+from user import User
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    elif request.method == 'POST':
+        values = (
+            None,
+            request.form['username'],
+            User.hash_password(request.form['password'])
+        )
+        User(*values).create()
+
+        return redirect('/')
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.find_by_username(username)
+        if not user or not user.verify_password(password):
+            return None
+        token = user.generate_token()
+        return jsonify({'token': token.decode('ascii')})
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ user.py
@@ -0,0 +1,46 @@
+import hashlib
+
+from database import DB
+
+
+SECRET_KEY = 'ncXZyx5cLR7x1$B^Ybtqp1f!E#dG4H3EN@ioYYKoxx'
+
+class User:
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+
+    def create(self):
+        with DB() as db:
+            values = (self.username, self.password)
+            db.execute('''
+                INSERT INTO users (username, password)
+                VALUES (?, ?)''', values)
+            return self
+
+
+    @staticmethod
+    def find_by_username(username):
+        if not username:
+            return None
+        with DB() as db:
+            row = db.execute(
+                'SELECT * FROM users WHERE username = ?',
+                (username,)
+            ).fetchone()
+            if row:
+                return User(*row)
+
+
+    @staticmethod
+    def hash_password(password):
+        return hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+
+    def verify_password(self, password):
+        return self.password == hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+
+
